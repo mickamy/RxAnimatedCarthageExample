6b31ba8caa344477ce2a8dda32a9c4c69516ed6d
mickamy <t.mikami1992@gmail.com>
2020-04-07 21:01:36 +0900

    Create project with Xcode

---
+++ RxAnimatedCarthageExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxAnimatedCarthageExample
+//
+//  Created by mickamy on 2020/04/07.
+//  Copyright © 2020 mickamy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
